f8706f1f6ed21c45fe91abd9cd2853c3eeb6d435
WPH <WPH@126.com>
2017-08-14 01:15:24 +0800

    0813浪了一天
    罪孽深重啊~

---
+++ 0812_良仓/js/ajax.js
@@ -0,0 +1,30 @@
+var $={
+    ajax:function(options){
+        var url=options.url;
+        if(url===undefined){
+            throw new Error("Ajax request url! Please check it!");
+            return;
+        }
+        var type= options.type ||"GET";
+        var success=options.success;
+
+        var xhr=new XMLHttpRequest();
+        xhr.onreadystatechange=function(){
+            if(xhr.readyState===4){
+                if(xhr.status>=200&&xhr.status<300||xhr.status===304){
+                    var json=JSON.parse(xhr.responseText);
+                    success(json);
+                }
+            }
+        }
+
+        xhr.open(type,url,true)
+        if(type==="POST"||type==="post"){
+            xhr.setRequestHeader("");
+            var content=options.content||"";
+        }else{
+            var content=null;
+        }
+        xhr.send(content);
+    }
+}
---
+++ 0812_良仓/js/login.js
@@ -0,0 +1,15 @@
+var oMobile=document.getElementById("mobile");
+var oCode=document.getElementById("code");
+var oGet=document.getElementById("get");
+
+
+
+
+$.ajax({
+    "type":"GET",
+    "url":"http",
+    "success":function(response){
+        
+    },
+    "content":"1", 
+});
